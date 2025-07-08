9099bb260d8be489eca0220371d0bd7a961d3ed4
rivacortez <rivaldo062134@gmail.com>
2025-07-08 00:48:24 -0500

    feat

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -58,14 +58,6 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
-      }}
-      components={{
-        IconLeft: ({ className }: { className?: string }) => (
-          <ChevronLeft className={cn("size-4", className)} />
-        ),
-        IconRight: ({ className }: { className?: string }) => (
-          <ChevronRight className={cn("size-4", className)} />
-        ),
       }}
       {...props}
     />
